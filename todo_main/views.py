bc4fbb83226890a49f7c6ae52231d10fdbabe591
kshitish18 <kshitishsandhibigraha1999@gmail.com>
2025-01-19 18:51:28 +0530

    ToDo app final touch

--- todo_main/views.py
+++ todo_main/views.py
@@ -5,7 +5,12 @@ from todo.models import Task
 
 def home(request):
     tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
+    completed_tasks = Task.objects.filter(is_completed=True)
+    uncompleted_tasks = Task.objects.filter(is_completed=False)
+    
     context = {
         'tasks': tasks,
+        'completed_tasks': completed_tasks,
+        'uncomplete_tasks': uncompleted_tasks,
     }
     return render(request, 'home.html', context)
